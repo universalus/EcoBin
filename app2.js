631c2ff8e4a5c8fa808eb6ada9f9ea799d766a39
Nikhil Anand <91376294+universalus@users.noreply.github.com>
2023-03-20 15:12:36 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,110 @@
+var map,marker2,marker3;
+function initMap1() {
+    map = new mappls.Map('map', {
+      center: [26.8416163,75.5612288],
+      zoomControl: true,
+      location: true,
+      geolocation:true
+    });
+
+    
+    map.addListener('load', function() {
+        var all_data2 = [];
+        for (let i = 0; i < 7500; i++) {
+            let lt = randomNumber(26.805813,26.908404 );
+            let ln = randomNumber( 75.519512,  75.592997);
+            obj = {
+                "type": "Feature",
+                "properties": {
+                    "htmlPopup": "MapmyIndia "+i+"", "icon-size1": 0.55
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [lt, ln]
+                }
+            }
+            all_data2.push(obj);
+        }
+    marker2 = new mappls.Marker({
+        map: map,
+        position: {
+            "id": "ravi",
+            "type": "FeatureCollection",
+            "features": all_data2
+        },
+        fitbounds: true,
+        icon: 'images/red.png',
+        offset: [0, -40],
+        draggable: false,
+        clustersOptions: {
+            background: [
+                [2, 'red'],
+                [5, 'violet'],
+                [10, 'yellow'],
+                [20, 'orange'],
+                [35, 'blue']
+            ], // steps in background color (in pixels)
+            maxZoom: 17, // that zoom for split cluster circle 
+            radius: [
+                [2, 15],
+                [5, 20],
+                [10, 30],
+                [20, 40],
+                [35, 50],
+            ] // steps in cluster radius (in pixels)
+        }
+    });
+});
+    map.addListener('load', function() {
+        var all_data3 = [];
+        for (let i = 0; i < 15000; i++) {
+            let lt = randomNumber(26.805813,26.908404 );
+            let ln = randomNumber( 75.519512,  75.592997);
+            obj = {
+                "type": "Feature",
+                "properties": {
+                    "htmlPopup": "MapmyIndia "+i+"", "icon-size1": 0.55
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [lt, ln]
+                }
+            }
+            all_data3.push(obj);
+        }
+    marker3 = new mappls.Marker({
+        map: map,
+        position: {
+            "id": "ravi",
+            "type": "FeatureCollection",
+            "features": all_data3
+        },
+        fitbounds: true,
+        icon: 'images/yellow.png',
+        offset: [0, -40],
+        draggable: false,
+        clustersOptions: {
+            background: [
+                [2, 'red'],
+                [5, 'violet'],
+                [10, 'yellow'],
+                [20, 'orange'],
+                [35, 'blue']
+            ], // steps in background color (in pixels)
+            maxZoom: 17, // that zoom for split cluster circle 
+            radius: [
+                [2, 15],
+                [5, 20],
+                [10, 30],
+                [20, 40],
+                [35, 50],
+            ] // steps in cluster radius (in pixels)
+        }
+    });
+});
+}
+
+// generate random lat lng    
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min
+}
---
+++ script.js
@@ -0,0 +1,65 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// Select tab content
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  // Add border to current tab
+  this.classList.add("tab-border");
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach(item => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+  tabContentItems.forEach(item => item.classList.remove("show"));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener("click", selectItem));
+
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+
+	$(function() {
+		var header = $(".start-style");
+		$(window).scroll(function() {    
+			var scroll = $(window).scrollTop();
+		
+			if (scroll >= 10) {
+				header.removeClass('start-style').addClass("scroll-on");
+			} else {
+				header.removeClass("scroll-on").addClass('start-style');
+			}
+		});
+	});		
+		
+	//Animation
+	
+	$(document).ready(function() {
+		$('body.hero-anime').removeClass('hero-anime');
+	});
+
+	//Menu On Hover
+		
+	$('body').on('mouseenter mouseleave','.nav-item',function(e){
+			if ($(window).width() > 750) {
+				var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+				setTimeout(function(){
+				_d[_d.is(':hover')?'addClass':'removeClass']('show');
+				},1);
+			}
+	});	
+	
+  })(jQuery);
+
+
+  
